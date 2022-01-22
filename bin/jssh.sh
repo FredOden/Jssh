fa9ff52a1b13da3f84fd458303f8e859cfc0d15a
Fred J. Oden <fred.oden@gmail.com>
2022-01-22 09:53:11 +0100

    jssh ready to run from anywhere

--- bin/jssh.sh
+++ bin/jssh.sh
@@ -1,2 +1,5 @@
 #/bin/sh
-java -cp ../libs/rhino-1.7.13.jar:./jssh.jar  Jssh $1
+JSSH_DIR=/home/pi/Lourah/JSF/jssh
+JSSH_BIN=$JSSH_DIR/bin
+JSSH_LIBS=$JSSH_DIR/libs
+java -cp $JSSH_LIBS/rhino-1.7.13.jar:$JSSH_BIN/jssh.jar  Jssh $1
