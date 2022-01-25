0710250d107924353a2b177a52758396ee244a67
Fred J. Oden <fred.oden@gmail.com>
2022-01-25 19:56:29 +0100

    corrected compile.sh to generate jar from gen (where are the .class files) directoy

--- src/compile.sh
+++ src/compile.sh
@@ -1,2 +1,5 @@
 #/bin/sh -v
-javac -Xlint:deprecation -d ../gen -cp ../libs/rhino-1.7.13.jar JsActivity.java Js.java Jssh.java && jar -cvmf ./MANIFEST.MF ../bin/jssh.jar ../gen/*.class
+javac -Xlint:deprecation -d ../gen -cp ../libs/rhino-1.7.13.jar JsActivity.java Js.java Jssh.java && (
+cd ../gen
+jar -cvmf ../src/MANIFEST.MF ../bin/jssh.jar *.class
+)
