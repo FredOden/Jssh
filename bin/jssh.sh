f896a36a7ff0e73c3ca31f4ddc432b603fecd8f4
Frederic Oden <fred.oden@gmail.com>
2026-02-15 18:37:40 +0100

    Upgraded to Rhino 1,7.15

--- bin/jssh.sh
+++ bin/jssh.sh
@@ -5,6 +5,6 @@ export JSSH_LIBS=$JSSH_DIR/libs
 export JSSH_FRAMEWORK_DIR=$JSSH_DIR/framework
 export JSSH_ASSETS=$JSSH_DIR/assets
 export JSSH_STARTER=$JSSH_ASSETS/jssh.js
-export JSSH_RHINO=rhino-1.7.13.jar
+export JSSH_RHINO=rhino-1.7.15.jar
 #java -cp "$JSSH_LIBS/$JSSH_RHINO:$JSSH_BIN/jssh.jar:$JSSH/gen" Jssh $1
 $JSSH_JAVA -cp $JSSH_LIBS/$JSSH_RHINO:$JSSH_BIN/jssh.jar Jssh $@
--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,5 @@
 #/bin/sh -v
-$JSSH_JAVAC -Xlint:deprecation -d ../gen -cp ../libs/rhino-1.7.13.jar JsActivity.java Js.java Jssh.java && (
+$JSSH_JAVAC -Xlint:deprecation -d ../gen -cp ../libs/rhino-1.7.15.jar JsActivity.java Js.java Jssh.java && (
 cd ../gen
 $JSSH_JAR -cvmf ../src/MANIFEST.MF ../bin/jssh.jar *.class
 )
